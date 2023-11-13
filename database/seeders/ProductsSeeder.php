9ecc99ac1f3ca4a223320095bd13e4a4ad9184f9
LuisHernandezITA <20151628@aguascalientes.tecnm.mx>
2023-11-12 23:41:53 -0600

    DOU

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -19,7 +19,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = "1";
         $products->price = "100";
         $products->available_stock = "100";
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/5tYK7yjC/snu1.jpg";
         $products->addition_date = '2023-10-12';
         $products->available = True;
         $products->save();
@@ -41,7 +41,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = "1";
         $products->price = "100";
         $products->available_stock = "100";
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/7LFsp4TQ/snu2.jpg";
         $products->addition_date = '2023-10-12';
         $products->available = True;
         $products->save();
@@ -52,7 +52,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = "1";
         $products->price = "100";
         $products->available_stock = "100";
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/pTKGvSfV/snu4.jpg";
         $products->addition_date = '2023-10-12';
         $products->available = True;
         $products->save();
@@ -63,7 +63,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = 1; // Shirts
         $products->price = 45;
         $products->available_stock = 50;
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/DwYYPLfh/snu3.jpg";
         $products->addition_date = '2023-10-15';
         $products->available = true;
         $products->save();
@@ -74,7 +74,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = 1; // Shirts
         $products->price = 120;
         $products->available_stock = 30;
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/SNk5Sm1B/snu5.jpg";
         $products->addition_date = '2023-10-14';
         $products->available = true;
         $products->save();
@@ -85,7 +85,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = 3; // Caps
         $products->price = 80;
         $products->available_stock = 75;
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/yxXbmGxp/snu6.jpg";
         $products->addition_date = '2023-10-13';
         $products->available = true;
         $products->save();
@@ -96,7 +96,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = 3; // Shirts
         $products->price = 60;
         $products->available_stock = 40;
-        $products->images = "https://i.postimg.cc/gjrrf1st/cap.png";
+        $products->images = "https://i.postimg.cc/6pQmZYXz/snu8.jpg";
         $products->addition_date = '2023-10-12';
         $products->available = true;
         $products->save();
@@ -107,7 +107,7 @@ class ProductsSeeder extends Seeder
         $products->category_id = 2; // Pants
         $products->price = 55;
         $products->available_stock = 60;
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/v8Wkw4h7/snu7.jpg";
         $products->addition_date = '2023-10-11';
         $products->available = true;
         $products->save();
@@ -118,11 +118,12 @@ class ProductsSeeder extends Seeder
         $products->category_id = 1; // Caps
         $products->price = 25;
         $products->available_stock = 90;
-        $products->images = "https://i.postimg.cc/MTp3yGPM/playeramm.png";
+        $products->images = "https://i.postimg.cc/ydR5dxHH/snu9.jpg";
         $products->addition_date = '2023-10-10';
         $products->available = true;
         $products->save();
 
         $products = new Products();
     }
+
 }
